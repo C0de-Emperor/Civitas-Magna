5fccd0643e93211f528e8b6b2228a15c951ffc42
ggglllkkk <129752757+ggglllkkk@users.noreply.github.com>
2025-11-20 10:38:08 +0100

    builder and settler working again

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -109,7 +109,7 @@ public class SelectionManager : MonoBehaviour
             showOverlay = !showOverlay;
         }
 
-        if (selectionOutline == null || innerSelectionOutline == null || !MapGenerator.instance.isMapReady || !canInteract || EventSystem.current.IsPointerOverGameObject())
+        if (selectionOutline == null || innerSelectionOutline == null || !MapGenerator.instance.isMapReady || !canInteract || PauseMenuManager.instance.isMenuOpen)
         {
             if (outlinedCell != null || selectedCell != null || selectedUnit != null)
             {
@@ -128,7 +128,7 @@ public class SelectionManager : MonoBehaviour
 
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity) && !EventSystem.current.IsPointerOverGameObject())
         {
             Vector2 coord = HexMetrics.CoordinateToOffset(hit.point.x, hit.point.z, grid.hexSize, grid.orientation);
             HexCell currentCell = grid.GetTile(coord);
@@ -191,8 +191,6 @@ public class SelectionManager : MonoBehaviour
         }
         else
         {
-            selectedCell = null;
-            selectedUnit = null;
             outlinedCell = null;
             selectionOutline.SetActive(false);
         }
